fb425002aa25129393e863b9019b86796f1d55c4
WWF2015 <874353174@qq.com>
2015-01-30 00:48:23 +0800

    修正了其中的乱码，并尝试实现任务列表与编辑任务间的数据传递

--- src/com/scnu/imutest/Aty_EditTask.java
+++ src/com/scnu/imutest/Aty_EditTask.java
@@ -6,147 +6,133 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.View;
+import android.view.*;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class Aty_EditTask extends Activity {
 	
-	//��������
-		@SuppressWarnings("unused")
-		private TextView theme;      //�����ʶ�����
-		@SuppressWarnings("unused")
-		private TextView time;       //��ֹʱ���ʶ�����
-		@SuppressWarnings("unused")
-		private TextView message;    //���ݱ�ʶ�����
-		private EditText m_theme;    //������������
-		private EditText m_time;     //��ֹʱ����������
-		private EditText m_message;  //������������
-		private String s_department;  //���������ַ���
-		private String s_theme;          //���������ַ���
-		private String s_time;            //�����ֹʱ���ַ���
-		private String s_message;     //���������ַ���
-		private Bundle bundle;
+	/*定义变量*/
+	private EditText m_theme;    //定义主题输入框变量
+	private EditText m_time;     //定义截止时间输入框变量
+	private EditText m_message;  //定义内容输入框变量
+	Data_TaskDistribute data;     //定义任务的数据变量
+	private Bundle bundle;         //定义Activity间传递数据的打包变量
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_edit_task);
-		/*���TextView����*/
-		theme=(TextView)findViewById(R.id.tvActivityID);
-		time=(TextView)findViewById(R.id.tvActivityTime);
-		message=(TextView)findViewById(R.id.tvActivityContext);
-		/*���EditText����*/
-		m_theme=(EditText)findViewById(R.id.tvActivityID);
-		m_time=(EditText)findViewById(R.id.tvActivityTime);
-		m_message=(EditText)findViewById(R.id.tvActivityContext);
-		
-		//receiveMessage();
-		
-		/*����EditText��ʾ������*/
-		m_theme.setHint("����������");
-		m_time.setHint("�������ֹʱ��");
-		m_message.setHint("��������������");
-		/*���Button����*/
-		final Button back=(Button)findViewById(R.id.btnReturnActivity);
-		final Button save=(Button)findViewById(R.id.btnSaveEditTask);
-		final Button next=(Button)findViewById(R.id.btnPersonnelArrange);
-		
-		/*����button�齨���¼�listener*/
-		back.setOnClickListener(backOnClick);
-		save.setOnClickListener(saveOnClick);
-		next.setOnClickListener(nextOnClick);
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+	setContentView(R.layout.activity_edit_task);
+	/*获得EditText对象*/
+	m_theme=(EditText)findViewById(R.id.evTaskTheme);
+	m_time=(EditText)findViewById(R.id.evTaskDealtime);
+	m_message=(EditText)findViewById(R.id.evTaskMessage);
+
+	/*设置EditText显示的文字*/
+	m_theme.setHint("请输入活动主题");
+	m_time.setHint("请输入截止时间");
+	m_message.setHint("请输入任务内容");
+	/*获得Button对象*/
+	final Button buttonReturnActivityList=(Button)findViewById(R.id.btnReturnActivity);
+	final Button buttonSaveAssignmentData=(Button)findViewById(R.id.btnSaveEditTask);
+	final Button buttonGoPeopleArrange=(Button)findViewById(R.id.btnPersonnelArrange);
+	
+	/*设置事件监听*/
+	buttonReturnActivityList.setOnClickListener(backOnClick);
+	buttonSaveAssignmentData.setOnClickListener(saveOnClick);
+	buttonGoPeopleArrange.setOnClickListener(nextOnClick);
+}
+
+/*获取前一个Activity传递过来的数据*/
+public void GetMessageFromAty_Activity()
+{
+	Bundle bundle=this.getIntent().getExtras();
+	
+	data.setTaskName(bundle.getString("KEY_CLUB"));
+	data.setTaskSubject(bundle.getString("KEY_THEME"));
+	data.setTaskcutofftime(bundle.getString("KEY_TIME"));
+	data.setTaskContent(bundle.getString("KEY_MESSAGE"));
+	
+	m_theme.setText(data.getTaskName());
+	m_time.setText(data.getTaskcutofftime());
+	m_message.setText(data.getTaskContent());
+}
+
+/*将Activity的数据打包并传递到下一个Activity*/
+public void GetMessageInThisActivity()
+{
+	data.setTaskSubject(bundle.getString(m_theme.getText().toString()));
+	data.setTaskcutofftime(bundle.getString(m_time.getText().toString()));
+	data.setTaskContent(bundle.getString(m_message.getText().toString()));
+	
+	bundle=new Bundle();
+	bundle.putString("KEY_CLUB",data.getTaskName());
+	bundle.putString("KEY_THEME",data.getTaskSubject());
+	bundle.putString("KEY_TIME",data.getTaskcutofftime());
+	bundle.putString("KEY_MESSAGE",data.getTaskContent());
+}
+/*保存按钮显示提醒*/
+//
+public void DisplayToast(String str)
+{
+	Toast toast=Toast.makeText(this,str,Toast.LENGTH_SHORT);
+	toast.setGravity(Gravity. TOP, 0,220);
+	toast.show();
+}
+
+//返回按钮响应函数
+private Button.OnClickListener backOnClick=new Button.OnClickListener()
+{
+	public void onClick(View u)
+	{
+		finish();
 	}
-	
-	/*���������һ��Activity���ݵĺ���*/
-	@SuppressWarnings("unused")
-	private void receiveMessage()
+};
+
+//保存按钮响应函数
+private Button.OnClickListener saveOnClick=new Button.OnClickListener()
+{
+	public void onClick(View v)
 	{
-		Bundle bundle=this.getIntent().getExtras();
-		
-		s_department=bundle.getString("KEY_DEPARTMENT");
-		s_theme=bundle.getString("KEY_THEME");
-		s_time=bundle.getString("S_TIME");
-		s_message=bundle.getString("KEY_MESSAGE");
-		
-		m_theme.setText(s_theme);
-		m_time.setText(s_time);
-		m_message.setText(s_message);
+		Intent intent=new Intent();
+		GetMessageInThisActivity();
+	    intent.putExtras(bundle);
+	    setResult(RESULT_OK,intent);
+		DisplayToast("保存任务信息成功");
 	}
-	
-	/*��ȡ�༭���ڵ����ݣ����������*/
-	public void getMessage()
+};
+
+//人员分配按钮响应函数
+private Button.OnClickListener nextOnClick=new Button.OnClickListener()
+{
+	public void onClick(View w)
 	{
-		s_theme=m_theme.getText().toString();
-		s_time=m_time.getText().toString();
-		s_message=m_message.getText().toString();
-		
-		bundle=new Bundle();
-		bundle.putString("KEY_DEPARTMENT",s_department);
-		bundle.putString("KEY_THEME",s_theme);
-		bundle.putString("KEY_TIME",s_time);
-		bundle.putString("KEY_MESSAGE",s_message);
+		AlertDialog.Builder nextDlg=new AlertDialog.Builder(Aty_EditTask.this);
+		nextDlg.setTitle("提醒");
+		nextDlg.setMessage("是否跳转到人员分配界面？");
+		nextDlg.setIcon(android.R.drawable.ic_dialog_info);
+		nextDlg.setCancelable(false);
+		nextDlg.setPositiveButton("否",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+					}
+				});
+		nextDlg.setNegativeButton("是", 
+				new DialogInterface.OnClickListener() {	
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Intent it=new Intent();
+						it.setClass(Aty_EditTask.this,Aty_PersonnelArrange.class);
+						GetMessageInThisActivity();
+						it.putExtras(bundle);
+						startActivity(it);	
+					}
+				});
+		nextDlg.show();
 	}
-	
-	public void DisplayToast(String str)
-	{
-		Toast toast=Toast.makeText(this,str,Toast.LENGTH_SHORT);
-		toast.setGravity(Gravity. TOP, 0,220);
-		toast.show();
-	}
-	
-	//���ذ�ť��Ӧ����
-	private Button.OnClickListener backOnClick=new Button.OnClickListener()
-	{
-		public void onClick(View u)
-		{
-			finish();
-		}
-	};
-	
-	//���水ť��Ӧ����
-	private Button.OnClickListener saveOnClick=new Button.OnClickListener()
-	{
-		public void onClick(View v)
-		{
-			Intent intent=new Intent();
-			getMessage();
-		    intent.putExtras(bundle);
-			DisplayToast("����������Ϣ�ɹ���");
-		}
-	};
-	
-	//��Ա������Ӧ����
-	private Button.OnClickListener nextOnClick=new Button.OnClickListener()
-	{
-		public void onClick(View w)
-		{
-			AlertDialog.Builder nextDlg=new AlertDialog.Builder(Aty_EditTask.this);
-			nextDlg.setTitle("����");
-			nextDlg.setMessage("�Ƿ���ת����Ա������棿");
-			nextDlg.setIcon(android.R.drawable.ic_dialog_info);
-			nextDlg.setCancelable(false);
-			nextDlg.setPositiveButton("��",
-					new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-						}
-					});
-			nextDlg.setNegativeButton("��", 
-					new DialogInterface.OnClickListener() {	
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							Intent it=new Intent();
-							it.setClass(Aty_EditTask.this,Aty_PersonnelArrange.class);
-							getMessage();
-							it.putExtras(bundle);
-							startActivity(it);	
-						}
-					});
-			nextDlg.show();
-		}
-	};
+};
 
 }
